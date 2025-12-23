2e9f9df3ec429b8ac65dff1589a3353dc39a4cca
Abhishek <Abhishekraut8697@gmail.com>
2025-12-23 20:07:19 +0530

    migrate db

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,5 +1,5 @@
 // src/lib/prisma.ts
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "../generated/prisma/client.js";
 
 class PrismaSingleton {
   private static instance: PrismaClient;
